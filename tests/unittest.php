5e015a5495231356503e3c3ed50afec170432bb4
newsworthy39 <newsworthy39@github.com>
2023-03-04 10:31:38 +0100

    Use App(), Fix test for deleteDatabase()

--- tests/unittest.php
+++ tests/unittest.php
@@ -2,25 +2,34 @@
 
 namespace redcathedral\tests;
 
-use redcathedral\phpmysqladminrest\mysqladmin;
 use PHPUnit\Framework\TestCase;
+use function redcathedral\phpmysqladminrest\App;
 
-final class UnitTest extends TestCase {
-
+final class UnitTest extends TestCase
+{
     private $mysqladmin;
 
-    public function setUp() : void {
-        $this->mysqladmin = new mysqladmin();
+    public function setUp(): void
+    {
+        try {
+            $this->mysqladmin = App()->get(\redcathedral\phpmysqladminrest\mysqladmin::class);
+            $this->assertIsObject($this->mysqladmin);
+        } catch (\League\Container\Exception\NotFoundException $ex) {
+            $this->assertFalse(true);
+        }
     }
-    public function testCanCreateDatabase() : void {
-        
-        $this->mysqladmin->createDatabase('testDatabase');
-        
-        $this->assertTrue($this->mysqladmin->hasDatabase('testDatabase'));
-        $this->assertFalse(!$this->mysqladmin->hasDatabase('testDatabase'));
+    public function testCanCreateDatabase(): void
+    {
+        $this->assertTrue($this->mysqladmin->createDatabase('testDatabase')->hasDatabase('testDatabase'));
     }
 
-    public function tearDown() : void {
-        $this->mysqladmin->deleteDatabase('testDatabase');
+    public function testCanDeleteDatabase(): void
+    {
+        $this->assertFalse($this->mysqladmin->deleteDatabase('testDatabase')->hasDatabase('testDatabase'));
+    }
+
+    public function tearDown(): void
+    {
+        $this->mysqladmin->close();
     }
 }
