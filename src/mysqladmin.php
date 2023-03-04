d25424137d96f0e2b775190b37c4ea24543f48f7
newsworthy39 <newsworthy39@github.com>
2023-03-04 10:31:23 +0100

    Introduced dependency injection and inversion.
    This commit, introduces the use of Containers and a single App(), that allows
    access to a system-wide configuration, to allow
    for dependency injection
    and secrets seperation.

--- src/mysqladmin.php
+++ src/mysqladmin.php
@@ -6,21 +6,22 @@ use mysqli;
 
 class mysqladmin
 {
-
     private $mysql_dbh;
-    public function __construct()
+    public function __construct(mysqli $mysql)
     {
-        $this->mysql_dbh = mysqli_connect('localhost', 'mysqladmin', 'superadmin');
+        $this->mysql_dbh = $mysql;
     }
 
     public function __destruct()
     {
-        $this->close();
+        if($this->mysql_dbh)
+            $this->close();
     }
 
-    public function createDatabase(string $dbname)
+    public function createDatabase(string $dbname) : mysqladmin
     {
         $this->mysql_dbh->query(sprintf("CREATE DATABASE `%s`", $dbname));
+        return $this;
     }
 
     public function hasDatabase(string $dbname): bool
@@ -29,20 +30,22 @@ class mysqladmin
         $result = $this->mysql_dbh->query(sprintf("select 1 from information_schema.SCHEMATA WHERE SCHEMA_NAME = '%s'", $dbname));
         
         if ($result) {
-            $dbsFound = 1;
+            $dbsFound = $result->num_rows;
             $result->free_result();
         }
 
-        return $dbsFound === 1;
+        return $dbsFound == 1;
     }
 
-    public function deleteDatabase(string $dbname)
+    public function deleteDatabase(string $dbname) : mysqladmin
     {
         $this->mysql_dbh->query(sprintf("DROP DATABASE `%s`", $dbname));
+        return $this;
     }
 
     public function close()
     {
         $this->mysql_dbh->close();
+        $this->mysql_dbh = null;
     }
 }
