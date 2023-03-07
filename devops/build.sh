3aee03782af411d6a26020b2d6057ef0b6439ba8
newsworthy39 <newsworthy39@github.com>
2023-03-07 19:19:11 +0100

    Auto add keys to .env

--- devops/build.sh
+++ devops/build.sh
@@ -2,6 +2,11 @@
 
 # Build RSA-keys if missing.
 if [ ! -f mykey.pub ]; then
-    openssl genrsa -out mykey.pem 1024
-    openssl rsa -in mykey.pem -pubout > mykey.pub
+    openssl genrsa -out "${PWD}/mykey.pem" 1024
+    openssl rsa -in "${PWD}/mykey.pem" -pubout > "${PWD}/mykey.pub"
+    sed -i "/JWT_PRIV_KEY=/d" "${PWD}/.env"
+    sed -i "/JWT_PUB_KEY=/d" "${PWD}/.env"
+    echo "JWT_PRIV_KEY=${PWD}/mykey.pem" >> "${PWD}/.env"
+    echo "JWT_PUB_KEY=${PWD}/mykey.pub" >> "${PWD}/.env"
+    
 fi
